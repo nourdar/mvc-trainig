bc80dd630a1915681b5e4931bef47a2b73e039e4
nourdar <the.za3im@gmail.com>
2017-06-16 04:04:25 +0200

    i create this page to test get function in my routing system

---
+++ Controller/nourController.php
@@ -0,0 +1,21 @@
+<?php
+namespace Controllger;
+class nourController {
+  public function __construct(){
+    echo "hi from home Controller";
+  }
+
+  public static function index(){
+    echo "hi index Nour";
+  }
+
+  public static function gachtou(){
+    echo "hi From gachtou method";
+  }
+
+  public static function edit(){
+    echo "hi From Edit Page [  ]";
+  }
+}
+
+?>
